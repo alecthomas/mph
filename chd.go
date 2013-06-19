a20d001ff78b69558d17c834b4269368cb15703f
Alec Thomas <alec@swapoff.org>
2013-06-18 21:48:09 -0400

    Add fast slicereader.

--- chd.go
+++ chd.go
@@ -70,41 +70,6 @@ func Read(r io.Reader) (*CHD, error) {
 	return Mmap(b)
 }
 
-// Read values and typed vectors from a byte slice without copying where possible.
-type sliceReader struct {
-	b          []byte
-	start, end uint64
-}
-
-func (b *sliceReader) Read(size uint64) []byte {
-	b.start, b.end = b.end, b.end+size
-	return b.b[b.start:b.end]
-}
-
-func (b *sliceReader) ReadUint64Array(n uint64) []uint64 {
-	b.start, b.end = b.end, b.end+n*8
-	out := make([]uint64, n)
-	buf := b.b[b.start:b.end]
-	for i := 0; i < len(buf); i += 8 {
-		out[i>>3] = binary.LittleEndian.Uint64(buf[i : i+8])
-	}
-	return out
-}
-
-func (b *sliceReader) ReadUint16Array(n uint64) []uint16 {
-	b.start, b.end = b.end, b.end+n*2
-	out := make([]uint16, n)
-	buf := b.b[b.start:b.end]
-	for i := 0; i < len(buf); i += 2 {
-		out[i>>1] = binary.LittleEndian.Uint16(buf[i : i+2])
-	}
-	return out
-}
-
-func (b *sliceReader) ReadInt() uint64 {
-	return uint64(binary.LittleEndian.Uint32(b.Read(4)))
-}
-
 // Alias the CHD structure over an existing byte region (typically mmapped).
 func Mmap(b []byte) (*CHD, error) {
 	c := &CHD{}
---
+++ slicereader_fast.go
@@ -0,0 +1,37 @@
+// +build 386 amd64 arm
+
+package mph
+
+import (
+	"encoding/binary"
+	"github.com/alecthomas/unsafeslice"
+)
+
+// Read values and typed vectors from a byte slice without copying where
+// possible. This implementation directly references the underlying byte slice
+// for array operations, making them essentially zero copy. As the data is
+// written in little endian form, this of course means that this will only
+// work on little-endian architectures.
+type sliceReader struct {
+	b          []byte
+	start, end uint64
+}
+
+func (b *sliceReader) Read(size uint64) []byte {
+	b.start, b.end = b.end, b.end+size
+	return b.b[b.start:b.end]
+}
+
+func (b *sliceReader) ReadUint64Array(n uint64) []uint64 {
+	b.start, b.end = b.end, b.end+n*8
+	return unsafeslice.Uint64SliceFromByteSlice(b.b[b.start:b.end])
+}
+
+func (b *sliceReader) ReadUint16Array(n uint64) []uint16 {
+	b.start, b.end = b.end, b.end+n*2
+	return unsafeslice.Uint16SliceFromByteSlice(b.b[b.start:b.end])
+}
+
+func (b *sliceReader) ReadInt() uint64 {
+	return uint64(binary.LittleEndian.Uint32(b.Read(4)))
+}
