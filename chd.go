852579ece991b28d3618f7063f87882d251a1500
Jille Timmermans <jille@hexon.nl>
2024-01-31 09:41:28 +0100

    Reduce the number of pointers created in Mmap()
    Just use ints pointing into the large byte slice.
    
    Mmap() creates two `[]byte`s per key value pair. This creates
    significant work for the garbage collector which has to scan through all
    of these in each mark phase. For my 2.7M keys, it adds 5.4M pointers to
    be scanned. By just using integers for offsets the garbage collector can
    ignore it completely.
    
    This does trade off some more work for the Builder which has to append
    all the keys+values into a single byte slice.

--- chd.go
+++ chd.go
@@ -44,8 +44,14 @@ type CHD struct {
 	// more than 2^16 hash functions O_o
 	indices []uint16
 	// Final table of values.
-	keys   [][]byte
-	values [][]byte
+	mmap   []byte
+	keys   []dataSlice
+	values []dataSlice
+}
+
+type dataSlice struct {
+	start uint64
+	end   uint64
 }
 
 func hasher(data []byte) uint64 {
@@ -68,7 +74,7 @@ func Read(r io.Reader) (*CHD, error) {
 
 // Mmap creates a new CHD aliasing the CHD structure over an existing byte region (typically mmapped).
 func Mmap(b []byte) (*CHD, error) {
-	c := &CHD{}
+	c := &CHD{mmap: b}
 
 	bi := &sliceReader{b: b}
 
@@ -82,17 +88,25 @@ func Mmap(b []byte) (*CHD, error) {
 
 	el := bi.ReadInt()
 
-	c.keys = make([][]byte, el)
-	c.values = make([][]byte, el)
+	c.keys = make([]dataSlice, el)
+	c.values = make([]dataSlice, el)
 
 	for i := uint64(0); i < el; i++ {
 		kl := bi.ReadInt()
 		vl := bi.ReadInt()
-		c.keys[i] = bi.Read(kl)
-		c.values[i] = bi.Read(vl)
+		c.keys[i].start = bi.pos
+		bi.pos += kl
+		c.keys[i].end = bi.pos
+		c.values[i].start = bi.pos
+		bi.pos += vl
+		c.values[i].end = bi.pos
 	}
 
 	return c, nil
+}
+
+func (c *CHD) slice(s dataSlice) []byte {
+	return c.mmap[s.start:s.end]
 }
 
 // Get an entry from the hash table.
@@ -109,11 +123,11 @@ func (c *CHD) Get(key []byte) []byte {
 	ti := (h ^ r) % uint64(len(c.keys))
 	// fmt.Printf("r[0]=%d, h=%d, i=%d, ri=%d, r=%d, ti=%d\n", c.r[0], h, i, ri, r, ti)
 	k := c.keys[ti]
-	if bytes.Compare(k, key) != 0 {
+	if bytes.Compare(c.slice(k), key) != 0 {
 		return nil
 	}
 	v := c.values[ti]
-	return v
+	return c.slice(v)
 }
 
 func (c *CHD) Len() int {
@@ -152,13 +166,13 @@ func (c *CHD) Write(w io.Writer) error {
 
 	for i := range c.keys {
 		k, v := c.keys[i], c.values[i]
-		if err := write(uint32(len(k)), uint32(len(v))); err != nil {
+		if err := write(uint32(k.end-k.start), uint32(v.end-v.start)); err != nil {
 			return err
 		}
-		if _, err := w.Write(k); err != nil {
+		if _, err := w.Write(c.slice(k)); err != nil {
 			return err
 		}
-		if _, err := w.Write(v); err != nil {
+		if _, err := w.Write(c.slice(v)); err != nil {
 			return err
 		}
 	}
@@ -171,7 +185,7 @@ type Iterator struct {
 }
 
 func (c *Iterator) Get() (key []byte, value []byte) {
-	return c.c.keys[c.i], c.c.values[c.i]
+	return c.c.slice(c.c.keys[c.i]), c.c.slice(c.c.values[c.i])
 }
 
 func (c *Iterator) Next() *Iterator {
